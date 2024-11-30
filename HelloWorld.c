ec07cb0a28bc91891b2c160d2e34d91b32667a4a
Farhan <farlam080908@gmail.com>
2024-11-30 18:20:01 +0100

    Made some changes to Hello World

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(){
 
@@ -15,15 +16,20 @@ int main(){
     x = 123;     //initialization
     int y = 123; //decleration + initialization
 
-    int age = 21; //integer(whole number)
-    float gpa = 2.05; //floating point number
-    char grade = 'C'; //single character
-    char name[] = "Bro"; //array of characters
+    int age = 21; //integer(whole number) %d
+    float gpa = 2.05; //floating point number %f
+    char grade = 'C'; //single character %c
+    char name[] = "Bro"; //array of characters %s
+    double d = 3.3987492878275; //more accuracy and capacity %lf
+
+    bool e = false; //1 byte (true or false) %d
 
     printf("Hello %s\n",name);
     printf("You are %d years old\n",age);
     printf("Your average grade is %c\n",grade);
     printf("Your gpa is %f\n",gpa);
+    printf("This is a random number %0.15lf\n",d);
+    printf("%d",e);
 
     return 0;
 }
