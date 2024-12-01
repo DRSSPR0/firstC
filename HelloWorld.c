45a8ceb5ed65dc7212b555624e3542657ced2dc6
Farhan <farlam080908@gmail.com>
2024-12-01 13:45:49 +0100

    Programmed a simple calculator

--- HelloWorld.c
+++ HelloWorld.c
@@ -289,6 +289,51 @@ int main(){
       else{
       printf("\n %c is not a valid unit of measurement", unit);
     }*/
+//____________________________________________________________________
+
+  // Programming a simple calculator
+
+    char operator;
+    double num1;
+    double num2;
+    double result;
+
+    printf("\nEnter an operator (+ - * /): ");
+    scanf("%c", &operator);
+
+    printf("Enter number 1: ");
+    scanf("%lf", &num1);
+
+    printf("Enter number 2: ");
+    scanf("%lf", &num2);
+
+    switch (operator)
+    {
+    case '+':
+        result = num1 + num2;
+        printf("\nresult: %.2lf", result);
+      break;
+    case '-':
+        result = num1 - num2;
+        printf("\nresult: %.2lf", result);
+      break;
+    case '*':
+        result = num1 * num2;
+        printf("\nresult: %.2lf", result);
+      break;
+    case '/':
+        result = num1 / num2;
+        printf("\nresult: %.2lf", result);
+      break;
+    
+    default:
+        printf("%c not valid", operator);
+    }
+//____________________________________________________________________
+
+  // logical operators = && (AND) checks if two condiotions are true
+
+    
 
     return 0;
 }
